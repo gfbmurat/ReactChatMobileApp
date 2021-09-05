2a46c2e753ce39045969e40856d38e09f15ccc4a
Murat <aydn25m@gmail.com>
2021-09-05 16:16:28 +0300

    logo değiştirildi

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -17,7 +17,12 @@ const Sidebar = ({ setShowModal, isOpen, setIsOpen }) => {
         <div className={`fixed lg:static inset-y-0 left-0 w-64 z-50 px-8 py-4 bg-white overflow-auto border-r-[1px] border-gray-300 lg:translate-x-0 transform 
             ${isOpen ? 'xs:hidden translate-x-0 ease-out transition-medium' : '-translate-x-full ease-in transition-medium'}`}>
             <div className="flex justify-between items-center">
-                <img src="./images/logo.svg" alt="Logo" className="w-9 h-9" />
+                {/* <img src="./images/logo.svg" alt="Logo" className="w-9 h-9" /> */}
+                <button className="text-blue-500">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-9 w-9" viewBox="0 0 20 20" fill="currentColor">
+                        <path fillRule="evenodd" d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z" clipRule="evenodd" />
+                    </svg>
+                </button>
                 <button className="text-gray-700 lg:hidden" onClick={toggleButon}>
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
