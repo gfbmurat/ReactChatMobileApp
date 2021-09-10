a423e914ee1d6a00abd16e91ab02a2ae3236c6bd
Murat <aydn25m@gmail.com>
2021-09-10 07:19:47 +0300

    packmanloader size 30px olarak ayarlandı

--- src/components/auth/Fallback.js
+++ src/components/auth/Fallback.js
@@ -14,7 +14,7 @@ const Fallback = () => {
 
     return (
         <div className="sweet-loading fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-            <PacmanLoader speedMultiplier={1} loading={loading} css={override} color={color} size={"25"} />
+            <PacmanLoader speedMultiplier={1} loading={loading} css={override} color={color} size={"30px"} />
         </div>
     )
 }
