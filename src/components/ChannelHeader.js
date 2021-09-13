11bd4c85c8ab3f1e594ee1006c09d5787bd55cb7
Murat <aydn25m@gmail.com>
2021-09-13 03:07:02 +0300

    channelheader search icon eklendi

--- src/components/ChannelHeader.js
+++ src/components/ChannelHeader.js
@@ -71,8 +71,8 @@ const ChannelHeader = ({ searchTerm, setSearchTerm }) => {
             </div>
             {/* <input autoComplete="off" className="xs:w-1/2 xs:focus:w-full xs:duration-300 shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Search Message" /> */}
 
-            <div className="flex space-x-1 group bg-red-500">
-                <button className="group-focus-within:hidden text-gray-500">
+            <div className="flex space-x-1 group">
+                <button className="group-focus-within:z-[-50] text-gray-500">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                     </svg>
--- src/components/Header.js
+++ src/components/Header.js
@@ -42,9 +42,9 @@ const Header = ({ isOpen, setIsOpen }) => {
     }
 
     return (
-        <header className="flex justify-between items-center py-3 border-b-2 border-gray-400 ">
+        <header className="flex justify-between items-center py-3 border-b-2 border-gray-400">
             <div className="flex">
-                <button className="text-gray-600 lg:hidden dark:text-gray-300" onClick={toggleButon}>
+                <button className={`text-gray-600 ${isOpen ? '' : 'lg:hidden'} dark:text-gray-300`} onClick={toggleButon}>
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
                     </svg>
@@ -54,7 +54,7 @@ const Header = ({ isOpen, setIsOpen }) => {
 
             {/* Icons */}
             <div className="flex items-center">
-                <button ref={themeRef} onClick={updateTheme} className="text-gray-500 mr-2" >
+                <button ref={themeRef} onClick={updateTheme} className="text-gray-500 mr-2 hover:animate-pulse" >
                     {
                         profile.theme === "light" ? <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
